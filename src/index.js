93e730567a48ab1f0e7a437a2b079c22f8a30f91
Jacob Bare <jacob@limit0.io>
2018-10-10 22:16:28 -0500

    Create MarketingCloudAuth class

---
+++ src/index.js
@@ -0,0 +1,88 @@
+const fetch = require('node-fetch');
+
+class MarketingCloudAuth {
+  /**
+   * @param {object} options
+   * @param {string} options.clientId The Marketing Cloud API ID
+   * @param {string} options.clientSecret The Marketing Cloud API Scecret
+   * @param {string} options.authUrl The Marketing Cloud auth URL (optional)
+   */
+  constructor({
+    clientId,
+    clientSecret,
+    authUrl = 'https://auth.exacttargetapis.com/v1/requestToken',
+  } = {}) {
+    if (!clientId || !clientSecret) {
+      throw new Error('The `clientId` and `clientSecret` options are required.');
+    }
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.authUrl = authUrl;
+  }
+
+  /**
+   * Retrieves an access token.
+   * Will only make a request if the token is falsy, expired, or the method
+   * forces the request.
+   *
+   * @param {boolean} options.force Whether to force a new auth token request.
+   */
+  async retrieve({ force = false } = {}) {
+    if (!this.token || this.hasExpired() || force) {
+      this.token = await this.request();
+    }
+    return this.token;
+  }
+
+  /**
+   * Invalidates the token.
+   */
+  invalidate() {
+    this.token = undefined;
+    return this;
+  }
+
+  /**
+   * Determines if the authentication token has expired.
+   * Considers a falsy token as expired.
+   */
+  hasExpired() {
+    if (!this.token) return true;
+    if (this.expiresIn && this.expiresIn <= process.hrtime()[0]) return true;
+    return false;
+  }
+
+  /**
+   * Executes an authentication request to Marketing Cloud.
+   *
+   * @private
+   * @returns {Promise<string>} The Marketing Cloud access token.
+   */
+  async request() {
+    const { clientId, clientSecret } = this;
+    const res = await fetch(this.authUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ clientId, clientSecret }),
+    });
+
+    const {
+      errorcode,
+      message,
+      accessToken,
+      expiresIn,
+    } = await res.json();
+
+    if (errorcode) {
+      const e = new Error(message || 'An unknown, fatal error occured.');
+      e.code = errorcode;
+      throw e;
+    }
+    if (expiresIn) {
+      this.expiresIn = process.hrtime()[0] + expiresIn;
+    }
+    return accessToken;
+  }
+}
+
+module.exports = MarketingCloudAuth;
